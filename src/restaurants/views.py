1667393251363ab451cd0e634fa3a69de3107820
skA <sskenner@gmail.com>
2018-06-08 16:47:56 -0400

    7 - Render a Django Template

--- src/restaurants/views.py
+++ src/restaurants/views.py
@@ -1,18 +1,23 @@
+import random
 from django.http import HttpResponse
 from django.shortcuts import render
 
 # funcation based view
+# def home(request):
+#     html_var = 'f strings'
+#     html_ = f"""<!DOCTYPE html>
+#     <html lang=en>
+#     <head>
+#     </head>
+#     <body>
+#     <h1>sup world!</h1>
+#     <p>this is {html_var} coming through</p>
+#     </body>
+#     </html>
+#     """
+#     return HttpResponse(html_)
+#     # return render(request, "home.html", {})
+
 def home(request):
-    html_var = 'f strings'
-    html_ = f"""<!DOCTYPE html>
-    <html lang=en>
-    <head>
-    </head>
-    <body>
-    <h1>sup world!</h1>
-    <p>this is {html_var} coming through</p>
-    </body>
-    </html>
-    """
-    return HttpResponse(html_)
-    # return render(request, "home.html", {})
+    num = random.randint(0, 1000000000)
+    return render(request, "base.html", {"html_var": True, "num": num})
