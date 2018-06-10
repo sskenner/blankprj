335d29f3b3f1a404d526d3895028d76a8823ddd0
santana kenner <sskenner@gmail.com>
2018-06-10 22:50:59 +0000

    setup & configure for postgres

--- src/muypicky/settings/base.py
+++ src/muypicky/settings/base.py
@@ -75,10 +75,20 @@ WSGI_APPLICATION = 'muypicky.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'cfe_db',
+        'USER': 'sk',
+        'PASSWORD': '1234qwer',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
